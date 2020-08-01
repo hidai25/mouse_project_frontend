176c0ff59c540c781d93b2b26afe012d79e1b9cc
Hidai Bar-Mor <hidai25@gmail.com>
2020-08-01 16:23:44 +0300

    added about

--- src/components/the_team.js
+++ src/components/the_team.js
@@ -25,6 +25,13 @@ export default class Contact extends React.Component {
 		return (
 <GridWrapper>
 
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
 
 			   <Navbar fixed="top" />
 
